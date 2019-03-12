b21d0175f881705758f787a687c41ce12f56a741
Nanle Paul <paulnanle611@gmail.com>
2019-03-12 05:20:09 +0100

    Bugs Fixes
    Bugs has been fixed as stated in the mail that has been sent

---
+++ database/migrations/2019_03_12_035053_create_todolists_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodolistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todolist', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('userid');
+            $table->string('todo');
+            $table->integer('stats');
+            $table->date('timecreated');
+            $table->dateTimeTz('timecompleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todolist');
+    }
+}
